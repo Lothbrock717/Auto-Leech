a9d0b3cd867d1ded8a3024d2906b5f74200eb574
Lothbrock717 <91334582+Lothbrock717@users.noreply.github.com>
2026-03-14 23:01:46 +0530

    Update start.sh
    Signed-off-by: Lothbrock717 <91334582+Lothbrock717@users.noreply.github.com>

--- start.sh
+++ start.sh
@@ -1,1 +1,3 @@
-source .venv/bin/activate && python3 update.py && python3 -m bot
+apt-get install -y qbittorrent-nox 2>/dev/null || true
+pip install -r requirements.txt -q
+python3 update.py && python3 -m bot
